92a5d9aac78a41aeba26524c2c815a06abeefdd4
Yllies HADJ-ABED <Yllies@MacBook-Air-Yllies.local>
2023-05-04 17:14:35 +0200

    added return router

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./assets/pages/Home";
 
 function App() {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
